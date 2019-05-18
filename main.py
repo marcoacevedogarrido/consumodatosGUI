00f26f1e8b83014ba45d47b5aa1ed212dc22397f
MarcoAcevedoG <marco160195940@gmail.com>
2019-05-18 16:06:11 -0400

    lower

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ background_label = tk.Label(root, image=background)
 background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg='#13152C', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.60, relheight=0.1, anchor='n')
+frame.place(relx=0.5, rely=0.25, relwidth=0.6, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font=10)
 entry.place(rely=0.15, relwidth=0.65, relheight=0.7)
@@ -36,7 +36,7 @@ button = tk.Button(frame, text='Consultar', font=10, command=lambda: get_saldo(e
 button.place(relx=0.7, rely=0.12, relwidth=0.3, relheight=0.7)
 
 lower_frame = tk.Frame(root, bg='#13152C', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.6, relheight=0.6, anchor='n')
+lower_frame.place(relx=0.5, rely=0.1, relwidth=0.6, relheight=0.1, anchor='n')
 
 label = tk.Label(lower_frame)
 label.place(relwidth=1, relheight=1)
